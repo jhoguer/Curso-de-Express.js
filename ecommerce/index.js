a1ea24a75186b7b8c6f377d5ee76909611a69fcd
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-29 23:47:42 -0500

    Manejando archivos estaticos

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -5,7 +5,7 @@ const router = require('./routes/products');
 const app = express();
 
 
-
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
