b5f78882c883424d857fab6089b5462b1e117bd0
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-30 18:33:05 -0500

    Implementando el api de productos-dividiendo servicios

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const path = require('path');
-const router = require('./routes/products');
+const bodyParse = require('body-parser');
+const productsRouter = require('./routes/products');
+const productsApiRouter = require('./routes/api/products');
 
+// app
 const app = express();
 
+// middlewares
+app.use(bodyParse.json());
 
+// Static files
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 
-app.use('/products', router);
+// routes
+app.use('/products', productsRouter);
+app.use('/api/products', productsApiRouter )
+
+// redirect
+app.get('/', (req, res) => {
+  res.redirect('/products');
+});
 
 
-
+// server
 const server = app.listen(8000, () => {
   console.log(`Listening http://localhost:${server.address().port}`);
 })
---
+++ ecommerce/routes/api/products.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const ProductService = require('../../services/products');
+
+const router = express();
+
+
+const productService = new ProductService();
+
+router.get('/', async (req, res, next) => {
+  const { tags } = req.query;
+
+  try {
+    const products = await productService.getProducts({ tags });
+  
+    res.status(200).json({
+      data: products,
+      message: 'Products listed'
+    })
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.get('/:productId', (req, res, next) => {
+  const { productId } = req.params;
+
+  try {
+    const product = productService.getProduct({ productId });
+  
+    res.status(200).json({
+      data: product,
+      message: 'Product retrieved'
+    })
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.post('/', async (req, res, next) => {
+  const { body: product } = req;
+
+  try {
+    const createProduct = await productService.createProduct({ product });
+  
+    res.status(201).json({
+      data: createProduct,
+      message: 'Product create'
+    })
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.put('/:productId', async (req, res, next) => {
+  const { productId } = req.params;
+  const { body: product } = req;
+
+  try {
+    const updatedProduct = await productService.updateProduct({ productId, product });
+  
+    res.status(200).json({
+      data: updatedProduct,
+      message: 'Product update'
+    })
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  const { productId } = req.params;
+
+  try {
+    const product = await productService.deleteProduct({ productId });
+  
+    res.status(200).json({
+      data: product,
+      message: 'Product deleted'
+    })
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+router.patch('/:productId', async (req, res, next) => {
+  const { productId } = req.params;
+  const { body: product } = req;
+
+  try {
+    const patchedProduct = await productService.patchProduct({ productId, product });
+  
+    res.status(200).json({
+      data: patchedProduct,
+      message: 'Product patched'
+    })
+
+  } catch(err) {
+    next(err)
+  }
+
+})
+
+module.exports = router;
