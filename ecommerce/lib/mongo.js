5633de95fea93a8859f28fd232e31baa4641923d
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-30 22:05:13 -0500

    Conectando nuestros servicios con mongoDB

---
+++ ecommerce/lib/mongo.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+const { config } = require('../config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dBHost}/${DB_NAME}?retryWrites=true&w=majority`;
+
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.client.connect(error => {
+        if(error) {
+          reject(error);
+        }
+
+        console.log("Connected successfully to mongo!");
+        resolve(this.client.db(this.dbName));
+      });
+    });
+  }
+
+  getAll(collection, query) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).find(query).toArray();
+      });
+  }
+}
+
+module.exports = MongoLib;
